4ecccd85b3a01b3b0366c1d3e33b614b6fa4253d
Ronald Bogdanoff <ron@Ronalds-MacBook-Pro.local>
2016-06-16 11:55:47 -0700

    unit test for sourcebase needed a fake EC2Connection object.

--- README.md
+++ README.md
@@ -5,6 +5,8 @@
 An AWS interface/toolkit based on ReactiveX
 ===========================================
 
+This project is in a 'building up' phase!!
+
 The goal of this project is to create a toolkit lib that will provide an interface to the AWS API using ReactiveX http://reactivex.io/ https://github.com/ReactiveX/RxPY
 
 The initial idea is provide some 'out of the box' multi-step AWS utilities that uses reactive style programming.  The 'components' used to build the 'out of the box' multi-step utils could be reused to create other AWS utilities.
--- tests/unit/test_source/test_sourcebase.py
+++ tests/unit/test_source/test_sourcebase.py
@@ -2,11 +2,20 @@ from nose.tools import *
 from unittest.mock import patch, Mock
 from rxaws.source.sourcebase import SourceBase
 from boto.ec2.connection import EC2Connection
+import boto.ec2
+
+
+class UnitTestableEC2Connection(EC2Connection):
+	""" subclass EC2Conection that won't really do anything """
+	def __init__(self):
+		pass
 
 class TestSourceBase:
-	""" testing methods of an abstractclass.  Need to mock the @abstractmethods in this class """
+	# inject the bogus UnitTestableEC2Connection 
+	@patch('boto.ec2.connect_to_region', return_value=(UnitTestableEC2Connection()))
+	# mock the get_source_iterable abstractmethod
 	@patch.multiple(SourceBase, __abstractmethods__=set(), get_source_iterable=Mock(return_value=[1,2,3]))
-	def setup(self):
+	def setup(self, mock_connection):
 		# create instance of class under test
 		self.cut_sourcebase = SourceBase()
 
@@ -17,7 +26,9 @@ class TestSourceBase:
 		# when a new SourceBase instance is created
 		# is should contain an EC2Connection
 		assert self.cut_sourcebase.conn is not None
-		assert type(self.cut_sourcebase.conn) is EC2Connection
+		#isinstance(a, dict)
+		assert isinstance(self.cut_sourcebase.conn, EC2Connection) is True, \
+			'expected EC2Connection got: %s' % type(self.cut_sourcebase.conn)
 
 	@patch.multiple(SourceBase, __abstractmethods__=set(), get_source_iterable=Mock(return_value=[1,2,3]))
 	def test_iteration(self):
